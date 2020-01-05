013917507719f911ed3a3b8b482599ad5a866c01
oznoztn <goldenhawkk@gmail.com>
2020-01-05 19:10:02 +0300

    Define API endpoints for basic CRUD operations

--- server.js
+++ server.js
@@ -7,6 +7,39 @@ const app = express();
 
 dotenv.config({ path: './config/config.env' });
 
+app.get('/api/v1/movies', (req, res) => {
+  res.status(200).json({
+    message: 'Show all movies'
+  });
+});
+
+app.get('/api/v1/movies/:id', (req, res) => {
+  res.status(200).json({
+    message: `Show movie ${req.params.id}`
+  });
+});
+
+app.post('/api/v1/movies', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: 'Create a new movie'
+  });
+});
+
+app.put('/api/v1/movies/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: `Update movie ${req.params.id}`
+  });
+});
+
+app.delete('/api/v1/movies/:id', (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: `Delete movie ${req.params.id}`
+  });
+});
+
 app.listen(
   PORT,
   console.log(`The server is running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`)
