8d434e637b8df342a0d12aeb0e316f32a5141616
oznoztn <goldenhawkk@gmail.com>
2020-01-10 23:13:34 +0300

    Replace then/catch with async/await & try/catch

--- data/db.js
+++ data/db.js
@@ -1,20 +1,18 @@
 const mongoose = require('mongoose');
 
-const connectDB = () => {
-  // 'connect' method returns a promise
-  const conn = mongoose
-    .connect(process.env.MONGO_URI, {
+const connectDB = async () => {
+  // 'connect' method returns a promise so I can use async/await pattern
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false,
       useUnifiedTopology: true
-    })
-    .then(c => {
-      console.log(`MongoDB Connected: ${c.connection.host}`);
-    })
-    .catch(error => {
-      console.log(error);
     });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connectDB;
