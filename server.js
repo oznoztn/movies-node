c4cc7daacc19b5fe3c20509e66ce221116a67d0b
oznoztn <goldenhawkk@gmail.com>
2020-01-12 00:01:06 +0300

    Connect body-parser to express server pipeline

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./data/db');
+const bodyParser = require('body-parser');
 
 // Load environment variables
 dotenv.config({ path: './config/config.env' });
@@ -15,6 +16,9 @@ const movies = require('./routes/movies');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+// Body parser
+app.use(bodyParser.json());
 
 // Morgan HTTP request logger in dev env
 if (process.env.NODE_ENV == 'development') {
