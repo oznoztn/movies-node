fdbdd35cdf277ee0dc1f98692314c53e3ede31db
oznoztn <goldenhawkk@gmail.com>
2020-01-13 22:54:42 +0300

    Pass the next args to respective ctrler methods

--- routes/movies.js
+++ routes/movies.js
@@ -4,23 +4,23 @@ const router = express.Router();
 const movieController = require('../controllers/movie.controller');
 
 router.get('/', (req, res, next) => {
-  movieController.movie_list(req, res);
+  movieController.movie_list(req, res, next);
 });
 
 router.get('/:id', (req, res, next) => {
-  movieController.movie_get(req, res);
+  movieController.movie_get(req, res, next);
 });
 
 router.post('/', (req, res, next) => {
-  movieController.movie_create(req, res);
+  movieController.movie_create(req, res, next);
 });
 
 router.put('/:id', (req, res, next) => {
-  movieController.movie_update(req, res);
+  movieController.movie_update(req, res, next);
 });
 
 router.delete('/:id', (req, res, next) => {
-  movieController.movie_delete(req, res);
+  movieController.movie_delete(req, res, next);
 });
 
 // export the router
