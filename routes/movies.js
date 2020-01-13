9853deb703ac6989aed9b0e91905bd76239112f4
oznoztn <goldenhawkk@gmail.com>
2020-01-13 22:34:20 +0300

    Define the next param in movie.router methods

--- routes/movies.js
+++ routes/movies.js
@@ -3,23 +3,23 @@ const router = express.Router();
 
 const movieController = require('../controllers/movie.controller');
 
-router.get('/', (req, res) => {
+router.get('/', (req, res, next) => {
   movieController.movie_list(req, res);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', (req, res, next) => {
   movieController.movie_get(req, res);
 });
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   movieController.movie_create(req, res);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', (req, res, next) => {
   movieController.movie_update(req, res);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', (req, res, next) => {
   movieController.movie_delete(req, res);
 });
 
