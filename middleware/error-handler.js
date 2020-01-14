4fe59141237f44fb0168461da98ac1bb9e60ad80
oznoztn <goldenhawkk@gmail.com>
2020-01-14 21:58:35 +0300

    Catch CastError in errorHandler middleware
    We are to catch prospective errors here.
    So we won't bother throwing a new error by passing new ErrorResponse obj to next() method in controller methods catch blocks anymore.

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,4 +1,23 @@
-const errorHandler = (error, req, res, next) => {
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+  let error = { ...err };
+
+  // Why do I need to do this? Why spread operator doesn't 'spread' the message prop?
+  error.message = err.message;
+
+  switch (error.name) {
+    case 'CastError':
+      const id = error.value;
+      const message = `Movie not found with given id of ${id}`;
+
+      // It's actually a server error but user does not care if it is a server error
+      // The user cares only if the requested resource is available or not.
+      // And we say NO. It is not.
+      error = new ErrorResponse(message, 404);
+      break;
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error'
